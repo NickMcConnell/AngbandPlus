7ac1fa1d8a6994ca1c8cdca021b7974a0ac9a062
Chris Kousky <chriskousky@hotmail.com>
2011-07-24 08:23:50 -0600

    Slingmaster

--- src/defines.h
+++ src/defines.h
@@ -58,7 +58,7 @@
 #define FAKE_VERSION   0
 #define FAKE_VER_MAJOR 10
 #define FAKE_VER_MINOR 0
-#define FAKE_VER_PATCH 44
+#define FAKE_VER_PATCH 43
 
 
 /*
@@ -76,8 +76,8 @@
  */
 #define H_VER_MAJOR 0
 #define H_VER_MINOR 0
-#define H_VER_PATCH 44
-#define H_VER_EXTRA 0
+#define H_VER_PATCH 43
+#define H_VER_EXTRA 1
 
 
 #define ANGBAND_2_8_1
